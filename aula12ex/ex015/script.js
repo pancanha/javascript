4ef806c243fa575b85fcc22df110908f10daf3f1
pancanha <dioniziojonathan@gmail.com>
2023-05-18 06:47:03 -0300

    ex parte 3

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -1,4 +1,4 @@
-function verificar()
+function verificar() {
 var data = new Date()
 var ano = data.getFullYear()
 var fano = document.getElementById('txtano')
@@ -6,5 +6,45 @@ var res = document.querySelector('div#res')
 if (fano.value.length == 0 || fano.value > ano) {
     window.alert(`[ERRO] Verifique os dados e tente novamente!`)
 } else {
-    window.alert(`Tudo ok`)
+    var fsex = document.getElementsByName ('radsex')
+    var idade = ano - Number(fano.value)
+    var gênero = ''
+    var img = document.createElement ('img')
+    img.setAttribute('id', 'foto')
+    if (fsex[0].checked) {
+        gênero = 'Homem'
+        if (idade >=0 && idade <10) {
+            //Criança
+            img.setAttribute('src', 'bbmenino.jpg')
+        } else if (idade <21) {
+            //Jovem
+            img.setAttribute('src','jmenino.jpg')
+        } else if (idade <50) {
+            //Adulto
+            img.setAttribute('src','ahomem.jpg')
+        } else {
+            //Idoso
+            img.setAttribute('src','vhomem.jpg')
+        }
+    } else if (fsex[1].checked) {
+        gênero = 'Mulher'
+        if (idade >=0 && idade <10) {
+            //Criança
+            img.setAttribute('src', 'bbmenina.jpg')
+        } else if (idade <21) {
+            //Jovem
+            img.setAttribute('src','jmenina.jpg')
+        } else if (idade <50) {
+            //Adulto
+            img.setAttribute('src','amulher.jpg')
+        } else {
+            //Idoso
+            img.setAttribute('src','vmulher.jpg')
+        }
+    }
+    res.style.textAlign = 'center'
+    res.innerHTML = `Detectamos ${gênero} com ${idade} anos`
+    res.appendChild(img)
+}
+
 }
